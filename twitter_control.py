7dfad14515cb4d11af1494eef8305022badf7151
John Wang <wangjohn@mit.edu>
2012-02-08 03:12:35 -0800

    created database infrastructure

--- twitter_control.py
+++ twitter_control.py
@@ -1,32 +1,119 @@
 import twitter
+import sqlite3
+import Queue
+import unicodedata
 
+class Controller(object):
+    """Object for controlling and obtaining data from the twitter streams. 
+    Takes this data then puts it into a SQL database."""
+    def __init__(self, track_word_list, dbname="twitterdb"):
+        self.track_word_list = track_word_list
+        self.api = twitter.Api()
+        self.db = sqlite3.connect(dbname)
+        self.cursor = self.db.cursor()
+        self.create_sql_database()
+
+    def get_all_wordlist_statuses(self):
+        additions = []
+        for i in xrange(len(self.track_word_list)):
+            word = self.track_word_list[i]
+            additions.append(self.get_statuses(word))
+        new_additions = [(additions[i], self.track_word_list[i]) for i in \
+                xrange(len(self.track_Word_list))]
+        print new_additions
+
+    def get_statuses(self, word):
+        NoError = True
+        i = 1
+        count = 0
+        new_additions = 0
+        while NoError:
+            try:
+                search = self.api.GetSearch(word, per_page=100, page=i)
+                count += len(search)
+                new_additions += self.db_insert_search(search)
+                print word, i, len(search)
+            except twitter.TwitterError:
+                NoError = False
+            i += 1
+        return new_additions
+
+    def create_sql_database(self):
+        try:
+            self.cursor.execute('''create table twitterdb(status_id int primary key, datetime text, 
+                   msg_text text, location text, user_id text,
+                   user_screen_name text,
+                   user_location text, user_followers_count int, 
+                   user_statuses_count int, user_friends_count int)''')
+            self.db.commit()
+        except sqlite3.OperationalError:
+            pass
+
+    def db_insert_search(self, search):
+        new_additions = 0 
+        unfinished = Queue.Queue()
+        for s in search:
+            unfinished.put(s)
+        while not unfinished.empty():
+            status = unfinished.get()
+            user = status.user
+            data_tuple = (status.id, status.created_at, 
+                    status.text, status.location, user.id, user.screen_name,
+                    user.location, user.followers_count,
+                    user.statuses_count, user.friends_count)
+            try:
+                self.cursor.execute('''insert into twitterdb values 
+                        (?,?,?,?,?,?,?,?,?,?)''', \
+                        data_tuple)
+                unfinished.task_done()
+                self.db.commit()
+                new_additions += 1
+            except sqlite3.OperationalError:
+                unfinished.put(status)
+                unfinished.task_done()
+            except sqlite3.IntegrityError:
+                pass
+        return new_additions
+
+class AnalyzeDatabase(object):
+    """Class for analyzing and performing queries on the database."""
+    def __init__(self, dbname="twitterdb"):
+        self.db = sqlite3.connect(dbname)
+        self.cursor = self.db.cursor()
+    
+    def select_text(self):
+        for row in self.db.execute(\
+                "select msg_text, status_id from twitterdb"):
+            words = unicodedata.normalize('NFKD', row[0]).encode('ascii',\
+                    'ignore')
+            print words
+
+    def select_duplicates(self, category="status_id"):
+        for row in self.db.execute(\
+                "select status_id, count(status_id) as numoccurrences from twitterdb group by status_id having (count(status_id) > 1)"):
+            print row
+
+
+def test():
+    api = twitter.Api()
+    statuses = api.GetPublicTimeline()
+    print [s.created_at for s in statuses]
+    print [s.id for s in statuses]
+    print [s.text for s in statuses]
+    print [s.location for s in statuses]
+    print [s.user.screen_name for s in statuses]
+    print [s.user.location for s in statuses]
+    print [s.user.followers_count for s in statuses]
+    print [s.hashtags for s in statuses]
 
 if __name__ == '__main__':
-    api = twitter.Api()
-    statuses = api.GetPublicTimeline(500)
-    userlist = [s.user.name for s in statuses]
-    print 'User List Length: ', len(userlist)
-
-
-    status_dic = {}
-    NoError = True
-    i = 1
-    total_repeats = 0
-    num_repeats = 0
-    while NoError:
-        try: 
-            search = api.GetSearch('economy', per_page = 100, page=i)
-            print 'Search List Length: ', len(search)
-            for s in search:
-                if s.id in status_dic:
-                    total_repeats += 1
-                    if s.text == status_dic[s.id]: num_repeats += 1
-                else:
-                    status_dic[s.id] = s.text
-
-        except twitter.TwitterError:
-            NoError = False
-        
-        i += 1
-    print total_repeats, num_repeats
+    #test()
+   
+    #a = AnalyzeDatabase()
+    #a.select_duplicates()
+    word_list = ['economy', 'jobs', 'finance', 'recession', 'stock market']
+    control = Controller(word_list)
+    control.get_all_wordlist_statuses()
+    a = [len(control.status_dic_list[i]) for i in xrange(len(control.status_dic_list))]
+    print a
 
